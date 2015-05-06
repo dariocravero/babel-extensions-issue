b5ce0e3812a7ea2ea40efddc7b3632db068f4a25
Darío Javier Cravero <dario@uxtemple.com>
2015-05-06 16:12:01 +0100

    initial commit

---
+++ file2.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ImES6 = function ImES6() {
+  _classCallCheck(this, ImES6);
+};
+
+exports["default"] = ImES6;
+module.exports = exports["default"];
